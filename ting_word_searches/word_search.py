a483fe407ecdcda6c3432a17e3ad405f5f6e545a
Gustavo <gustavodarocha241@gmail.com>
2023-05-25 07:12:29 -0300

    feat: requisitos 7 e 8 completos

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,57 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    index = 1
+
+    for i in instance.queue:
+        my_dict = {
+                    "palavra": word,
+                    "arquivo": i["nome_do_arquivo"],
+                    "ocorrencias": []
+                }
+
+        for line in i["linhas_do_arquivo"]:
+            if word in line.lower():
+                my_dict["ocorrencias"].append(
+                        {
+                            "linha": index
+                         }
+                    )
+                index += 1
+
+            else:
+                index += 1
+
+        if len(my_dict["ocorrencias"]) != 0:
+            result.append(my_dict)
+
+        return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    index = 1
+
+    for i in instance.queue:
+        my_dict = {
+                    "palavra": word,
+                    "arquivo": i["nome_do_arquivo"],
+                    "ocorrencias": []
+                }
+
+        for line in i["linhas_do_arquivo"]:
+            if word in line.lower():
+                my_dict["ocorrencias"].append(
+                        {
+                            "linha": index,
+                            "conteudo": line
+                         }
+                    )
+                index += 1
+
+            else:
+                index += 1
+
+        if len(my_dict["ocorrencias"]) != 0:
+            result.append(my_dict)
+
+        return result
